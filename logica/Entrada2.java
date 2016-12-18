ca241b378ab91345b109b7a1ee0d5c043f261805
Marcos Ribeiro <marcossrj90@gmail.com>
2016-12-18 17:39:14 -0200

    Aula 12 - Entrada de dados com a classe JOptionPane

---
+++ logica/Entrada2.java
@@ -0,0 +1,15 @@
+/**
+ *Recuperar de dados que o usuario digita utilizando a classe JOptionPane
+ *@author Marcos Ribeiro
+ */
+
+import javax.swing.JOptionPane;
+
+ public class Entrada2 {
+	
+	public static void main (String[] args){
+		
+		String nome = JOptionPane.showInputDialog("Qual o seu nome?");
+		JOptionPane.showMessageDialog(null, "Bem Vindo " + nome + "!");
+	}
+}
