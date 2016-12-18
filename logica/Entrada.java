916c1c128b6ca6c25c6147353e54a34e089b6732
Marcos Ribeiro <marcossrj90@gmail.com>
2016-12-18 16:55:48 -0200

    Aula 11 - Entrada de Dados

---
+++ logica/Entrada.java
@@ -0,0 +1,21 @@
+/**
+ *Recuperar de dados que o usuario digita utilizando a classe Scanner
+ *@author Marcos Ribeiro
+ */
+
+import java.util.Scanner;
+
+ public class Entrada {
+	
+	public static void main (String[] args){
+		
+		//Recuperando os dados na chamada do metodo (nesse caso do metodo main)
+		//System.out.println(args[0]);
+		
+		//Interagindo com o usuario
+		Scanner s = new Scanner(System.in);
+		System.out.println("Qual o seu nome?");
+		String nome = s.nextLine();
+		System.out.println("Bem Vindo " + nome + "!");
+	}
+}
