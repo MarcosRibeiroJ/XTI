d9a2543c752e0a24b9ba350e4533d1763bf18d49
Marcos Ribeiro <marcossrj90@gmail.com>
2016-12-18 15:06:23 -0200

    Aula 9 - Constantes

--- logica/Variavel.java
+++ logica/Variavel.java
@@ -12,6 +12,17 @@ public class Variavel {
 		int idade = 26; //variavel primitiva, na memoria do computador esta armazenado exatamente o valor de 26 em binarios
 		boolean solteiro = false; // variavel primitiva, esta armazenado 0 na memoria
 		
+		
+		//Contantes em java devem iniciar com o modificador final, o nome deve ser todo em maiuscula e caso seja nome composto, deve ser separado por underline.
+		//Caso eu tente atribuir outro valor a uma constante o programa nao ira compilar.
+		
+		//Abaixo, temos 3 exemplos de constantes:
+		final char SEXO_MASCULINO = 'M';
+		final char SEXO_FEMININO = 'F';
+		final double PI = 3.14;
+		
+		
 		System.out.println(nome);
+		System.out.println(PI);
 	}
 }
