022e9448d9d8d434427fe46e440cda9b877dcdfd
Marcos Ribeiro <marcossrj90@gmail.com>
2016-12-20 21:30:13 -0200

    Aula 19 - Desafio IMC com a classe Scanner

---
+++ logica/IMCScanner.java
@@ -0,0 +1,27 @@
+/**
+ *Calcular o IMC (Indice de Massa Comporal)
+ *IMC = pesoEmQuilogramas / (alturaEmMetros * alturaEmMetros)
+ *@author Marcos Ribeiro
+ */
+
+import java.util.Scanner;
+ 
+public class IMCScanner {
+	
+	public static void main (String[] args){
+	
+		Scanner s = new Scanner(System.in);
+		System.out.println("*** Sistema de calculo de IMC ***\n");
+		System.out.println("\nDigite seu peso: ");
+		double peso = s.nextDouble();
+		System.out.println("\nDigite sua altura: ");
+		double altura = s.nextDouble();
+		
+		double imc = peso / (altura * altura);
+		
+		String msg = (imc >= 20 && imc <= 25) ? "Peso ideal" : "Fora do peso ideal";
+		
+		System.out.println("\nResultado do IMC: " + msg);
+	}
+
+} 
