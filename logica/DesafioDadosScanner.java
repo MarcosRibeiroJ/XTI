e8e93a4b48c3318440a3dda5f0bd980bbf7ef45f
Marcos Ribeiro <marcossrj90@gmail.com>
2016-12-24 15:04:42 -0200

    Aula 25 - Desafio dos dados com Scanner

---
+++ logica/DesafioDadosScanner.java
@@ -0,0 +1,29 @@
+/**
+ *Desafio: Jogo que pessoa da um palpite sobre um numero (0-6) e o programa retorna se a pessoa acertou ou errou
+ *Utilizando a classe Scanner
+ *@author Marcos Ribeiro
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class DesafioDadosScanner {
+ 
+	public static void main (String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		Random r = new Random();
+		
+		int numeroDado = r.nextInt(6) + 1;
+		
+		System.out.println("*** Jogo do Palpite ***\n\n");
+		System.out.println("Favor digitar um numero do dado (de 1 ate 6): ");
+		int palpite = s.nextInt();
+		if (palpite == numeroDado) {
+			System.out.println("Parabens ... Voce acertou!!");
+		} else {
+			System.out.println("Que pena ... Voce errou o valor correto era " + numeroDado);
+		}
+	
+	}
+}
