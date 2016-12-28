f410e7747be705aaf37b8c0ab1c69f8dd0c9dc40
Marcos Ribeiro <marcossrj90@gmail.com>
2016-12-28 00:12:52 -0200

    Aula 29 - Fibonacci

---
+++ logica/Fibonacci.java
@@ -0,0 +1,21 @@
+/**
+ * Exercicio para imprimir a sequencia de Fibonacci
+ *@author Marcos Ribeiro
+ */
+
+public class Fibonacci {
+
+	public static void main (String[] args) {
+	
+		int i = 1;
+		int aux = 0;
+		
+		for(int j = 0; j < 50; j = aux){
+			System.out.print(j + "  ");
+			aux = i + j;
+			i = j;
+		}
+		
+	}
+
+}
