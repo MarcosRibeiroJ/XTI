44d15f065f73e200cd10a3968d49208514fd69aa
Marcos Ribeiro <marcossrj90@gmail.com>
2017-01-03 21:59:02 -0200

    Aula 34 - Orientacao a Objetos (Classe Cachorro)

---
+++ logica/Cachorro.java
@@ -0,0 +1,14 @@
+/**
+ * Primeiro teste de criacao de uma classe
+ *@author Marcos Ribeiro
+ */
+
+public class Cachorro {
+
+	int tamanho;
+	String raca;
+	
+	void latir(){
+		System.out.println("Au, Au, Au!");
+	}
+}
---
+++ logica/CachorroTest.java
@@ -0,0 +1,22 @@
+/**
+ * Classe para instanciar um objeto do tipo cachorro
+ *@author Marcos Ribeiro
+ */
+
+public class CachorroTest {
+
+	public static void main(String[] args) {
+		
+		Cachorro pitbull = new Cachorro();
+		pitbull.raca = "Pit Bull";
+		pitbull.tamanho = 40;
+		pitbull.latir();
+		
+		Cachorro viralata = new Cachorro();
+		viralata.raca = "Vira-Lata";
+		viralata.tamanho = 30;
+		viralata.latir();
+
+	}
+
+}
