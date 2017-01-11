20a8bb90f74562c9b67fc66028cefc5199275d73
Marcos Ribeiro <marcossrj90@gmail.com>
2017-01-11 18:08:42 -0200

    Aula 40 - Metodos get e set (Encapsulamento)

--- br/com/xti/poo/Funcionario.java
+++ br/com/xti/poo/Funcionario.java
@@ -3,6 +3,7 @@ package br.com.xti.poo;
 /**
  * Classe para testar modificadores de acesso dentro da classe Funcionario
  * Private: Visivel somente dentro da classe; Package: visivel dentro do pacote; Public: visivel para todos
+ * Foram criados metodos get e set para as variaveis nome e ativo
  *@author Marcos Ribeiro 
  */
 
@@ -11,16 +12,22 @@ public class Funcionario {
 	private String nome;
 	String sobrenome;
 	public String cidade;
+	private boolean ativo;
 	
 	public String getNome() {
 		return nome;
 	}
 	
-	public static void main(String[] args) {
-		
-		Funcionario f = new Funcionario();
-		f.nome ="Marcos";
-		
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public boolean isAtivo() {
+		return ativo;
+	}
+	
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
 	}
 
 }
--- br/com/xti/poo/FuncionarioPacote.java
+++ br/com/xti/poo/FuncionarioPacote.java
@@ -3,6 +3,7 @@ package br.com.xti.poo;
 /**
  * Classe para testar modificadores de acesso dentro do pacote da classe Funcionario 
  * Private: Visivel somente dentro da classe; Package: visivel dentro do pacote; Public: visivel para todos
+ * Foi testado o uso dos metodos get e set da variavel nome da classe Funcionario
  * @author Marcos Ribeiro
  */
 
@@ -11,7 +12,8 @@ public class FuncionarioPacote {
 	public static void main(String[] args) {
 		
 		Funcionario f = new Funcionario();
-		//f.nome = "Marcos"; nao consigo acessar porque esta private na classe Funcionario
+		String n = f.getNome();
+		f.setNome("Marcos");
 		
 		f.sobrenome = "Ribeiro"; //consigo acessar porque esta package na classe Funcionario
 
