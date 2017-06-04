318e50a1df7ed98892000c6b87203883cdd643cc
Marcos Ribeiro <marcossrj90@gmail.com>
2017-06-04 14:58:38 -0300

    Aula 73 - Classe Math

---
+++ br/com/xti/java/MathTest.java
@@ -0,0 +1,25 @@
+package br.com.xti.java;
+
+/**
+ * Classe criada para apresentar alguns metodos e constantes da classe Math
+ * @author Marcos Ribeiro
+ */
+
+public class MathTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.PI);//Constante PI
+		System.out.println(Math.E);//Constante Euler
+		
+		System.out.println(Math.sin(10));//seno
+		System.out.println(Math.cos(10));//cosseno
+		System.out.println(Math.tan(45));//tangente
+		System.out.println(Math.pow(4, 3));//potencia
+		System.out.println(Math.sqrt(16));//raiz quadrada
+		System.out.println(Math.cbrt(16));//raiz cubica
+		System.out.println(Math.hypot(6, 8));//hipotenusa
+
+	}
+
+}
